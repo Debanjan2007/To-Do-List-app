307319039f05393e35e9eb5bdd1d4ea06aabc9af
Debanjan Das <microsoft2420@gmail.com>
2025-06-09 22:59:25 +0530

    Update redis.db.js

--- backend/db/redis.db.js
+++ backend/db/redis.db.js
@@ -3,8 +3,8 @@ import { Redis } from '@upstash/redis'
 
 const client = new Redis({
     // not using the .env  file here for simplicity
-    url: "https://helpful-mouse-19959.upstash.io",
-    token: "AU33AAIjcDFlZGQ0OGU2YzAxZmI0ZWUxODIzNzVhMGNhMjVkZGRlMHAxMA"
+    url: "https://<your-redis-url>",
+    token: "<your-redis-token>"
 })
 
 
